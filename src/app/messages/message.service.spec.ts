de551ceb134d8a237df5981588fe9f0ee5336756
Manoj Garje <manoj.garje@agiliad.com>
2023-07-07 14:46:25 +0530

    CRUD application in angular

---
+++ src/app/messages/message.service.spec.ts
@@ -0,0 +1,26 @@
+import { MessageService } from "./message.service"
+
+describe('MessageService', () => {
+    let service: MessageService;
+
+    beforeEach(() => {
+        service = new MessageService();
+    })
+
+    it('should have no messages to start', () => {
+        expect(service.messages.length).toBe(0);
+    })
+
+    it('should add a message when add is called', () => {
+        service.addMessage('message1');
+
+        expect(service.messages.length).toBe(1);
+    })
+
+    it('should remove all message when clear is called', () => {
+        service.addMessage('message1');
+        service.clear();
+
+        expect(service.messages.length).toBe(0);
+    })
+})
